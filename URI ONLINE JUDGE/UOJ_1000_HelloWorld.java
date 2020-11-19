4a287bc8756b49b48e145ef467b0eadd55fb2c9b
Camilo Montoya Adc <camontoyah@unincca.edu.co>
2020-11-18 19:56:11 -0500

    Adición ejercicios iniciales
    Adición ejercicios template para inicializar el repositorio.

---
+++ URI ONLINE JUDGE/UOJ_1000_HelloWorld.java
@@ -0,0 +1,23 @@
+//Uri Online Judge: Ejercicio 1000 - Hello World!.
+//URL: https://www.urionlinejudge.com.br/judge/es/problems/view/1000
+//Elaborado en Java 1.7.
+//Answer: Accepted.
+//Time: 0.025s.
+//Memory: 183 Bytes.
+//Elaborado por: Camilo Montoya.
+
+//Import requerido para el manejo de excepciones.
+import java.io.IOException;
+
+//Clase main del programa. 
+public class Main {
+    
+    //Método main del programa.
+    public static void main(String[] args) throws IOException {
+        
+        //Impresión por consola del mensaje solicitado.
+        System.out.println("Hello World!");
+ 
+    }
+ 
+}
